5fc74da062a071345dcd71c74dffb2ca51a8bc5d
Branden <brandenehman45@gmail.com>
2021-06-17 12:32:05 -0600

    First Challenge done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log('%c HI', 'color: firebrick')
+
+const div = document.getElementById("dog-image-container");
+const ul = document.getElementById("dog-breeds");
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(function(resp) {
+    return (resp.json())
+})
+.then(function(resp) {
+    resp.message.forEach(cb => {
+        const newImg = document.createElement('img');
+        newImg.src = cb;
+        div.appendChild(newImg);
+    })
+});
+
+fetch('https://dog.ceo/api/breeds/list/all')
+.then(function(resp) {
+    return (resp.json())
+})
+.then(function(resp) {
+    resp.message.forEach(cb => {
+        const li = document.createElement('li');
+        li.value = cb;
+        ul.appendChild(li);
+    })
+})
