501db84f20709682a475ba2e3b94d165de3d7523
Branden <brandenehman45@gmail.com>
2021-06-17 14:06:18 -0600

    Finished second challenge

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ console.log('%c HI', 'color: firebrick')
 const div = document.getElementById("dog-image-container");
 const ul = document.getElementById("dog-breeds");
 
+
 fetch("https://dog.ceo/api/breeds/image/random/4")
-.then(function(resp) {
-    return (resp.json())
-})
+.then(resp => resp.json())
 .then(function(resp) {
     resp.message.forEach(cb => {
         const newImg = document.createElement('img');
@@ -15,14 +14,17 @@ fetch("https://dog.ceo/api/breeds/image/random/4")
     })
 });
 
+
 fetch('https://dog.ceo/api/breeds/list/all')
+.then(resp => resp.json())
 .then(function(resp) {
-    return (resp.json())
-})
-.then(function(resp) {
-    resp.message.forEach(cb => {
+    const keys = (Object.entries(resp.message))
+    keys.forEach(cb => {
         const li = document.createElement('li');
-        li.value = cb;
+        li.innerText = cb;
         ul.appendChild(li);
+        li.addEventListener('click', function(e) {
+            li.fontcolor("red")
+        })
     })
 })
