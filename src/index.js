d6102d23d1f0c1cdc8f885cdac9afe32309bc5d6
Branden <brandenehman45@gmail.com>
2021-06-20 16:28:39 -0600

    Getting closer to finishing dropdown

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ console.log('%c HI', 'color: firebrick')
 
 const div = document.getElementById("dog-image-container");
 const ul = document.getElementById("dog-breeds");
+const a = document.querySelector("#breed-dropdown").value = 'a';
+const b = document.querySelector("#breed-dropdown").value = 'b';
+const c = document.querySelector("#breed-dropdown").value = 'c';
+const d = document.querySelector("#breed-dropdown").value = 'd';
 
 
 fetch("https://dog.ceo/api/breeds/image/random/4")
@@ -14,17 +18,38 @@ fetch("https://dog.ceo/api/breeds/image/random/4")
     })
 });
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
-fetch('https://dog.ceo/api/breeds/list/all')
-.then(resp => resp.json())
-.then(function(resp) {
-    const keys = (Object.entries(resp.message))
-    keys.forEach(cb => {
-        const li = document.createElement('li');
-        li.innerText = cb;
-        ul.appendChild(li);
-        li.addEventListener('click', function(e) {
-            li.fontcolor("red")
+a.addEventListener('click', function(event) {
+        
+})
+
+function listBreeds() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(resp => resp.json())
+    .then(function(resp) {
+        const keys = (Object.keys(resp.message));
+        const startA = keys.filter(breed => breed.startsWith('a'))
+        const startB = keys.filter(breed => breed.startsWith('b'))
+        const startC = keys.filter(breed => breed.startsWith('c'))
+        const startD = keys.filter(breed => breed.startsWith('d'))
+        keys.forEach(cb => {
+            const li = document.createElement('li');
+            li.innerText = cb;
+            ul.appendChild(li);
+            li.addEventListener('click', function(e) {
+                li.style.color = getRandomColor();
+            });    
+        })
+        a.addEventListener('click', function(event) {
+
         })
     })
-})
+}
