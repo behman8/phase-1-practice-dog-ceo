063a4fc8979ddb69d51ba8973f2f13c22cdcc18a
Branden <brandenehman45@gmail.com>
2021-06-22 19:21:14 -0600

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 console.log('%c HI', 'color: firebrick')
 
+let breeds = []
+
+document.addEventListener('DOMContentLoaded', function (){
+    fetchDogs()
+    listBreeds()
+    dropdownEventListener()
+})
 const div = document.getElementById("dog-image-container");
-const ul = document.getElementById("dog-breeds");
 const a = document.querySelector("#breed-dropdown").value = 'a';
 const b = document.querySelector("#breed-dropdown").value = 'b';
 const c = document.querySelector("#breed-dropdown").value = 'c';
 const d = document.querySelector("#breed-dropdown").value = 'd';
-
-
-fetch("https://dog.ceo/api/breeds/image/random/4")
-.then(resp => resp.json())
-.then(function(resp) {
-    resp.message.forEach(cb => {
-        const newImg = document.createElement('img');
-        newImg.src = cb;
-        div.appendChild(newImg);
-    })
-});
+const ul = document.getElementById("dog-breeds");
+function fetchDogs() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(function(resp) {
+        resp.message.forEach(cb => {
+            const newImg = document.createElement('img');
+            newImg.src = cb;
+            div.appendChild(newImg);
+        })
+    });
+}
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -27,29 +34,41 @@ function getRandomColor() {
     return color;
   }
 
-a.addEventListener('click', function(event) {
-        
-})
-
 function listBreeds() {
     fetch('https://dog.ceo/api/breeds/list/all')
     .then(resp => resp.json())
     .then(function(resp) {
-        const keys = (Object.keys(resp.message));
-        const startA = keys.filter(breed => breed.startsWith('a'))
-        const startB = keys.filter(breed => breed.startsWith('b'))
-        const startC = keys.filter(breed => breed.startsWith('c'))
-        const startD = keys.filter(breed => breed.startsWith('d'))
-        keys.forEach(cb => {
-            const li = document.createElement('li');
-            li.innerText = cb;
-            ul.appendChild(li);
-            li.addEventListener('click', function(e) {
-                li.style.color = getRandomColor();
-            });    
-        })
-        a.addEventListener('click', function(event) {
+        breeds = (Object.keys(resp.message));
+        breeds.forEach(breed => {
+            addBreedToDom(breed)   
+         })
+    })
+}
 
-        })
+function addBreedToDom(breed) {
+    const li = document.createElement('li');
+    li.innerText = breed;
+    ul.appendChild(li);
+    li.addEventListener('click', function(e) {
+        li.style.color = getRandomColor();
+    })
+}
+
+function dropdownEventListener() {
+    const choice = document.querySelector("#breed-dropdown")
+    choice.addEventListener('change', function(event) {
+        filterBreed(event.target.value)
+    })
+}
+
+function filterBreed(letter) {
+    const result = breeds.filter(breed => breed.startsWith(letter))
+    updateBreedList(result)
+}
+
+function updateBreedList(allBreeds) {
+    ul.innerHTML = ' '
+    allBreeds.forEach(breed => {
+        addBreedToDom(breed)
     })
 }
